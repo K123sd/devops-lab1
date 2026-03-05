0930fc95af18e7c65762bddded70e9de7ee668ad
vamsi krishna <kvamsikrishna54321@gmail.com>
2026-03-05 19:38:17 +0000

    Addeed CI exit codes for disk usage

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-#echo "checking system Health";
-df -h;
-free -m;
+DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
+if [ "$DISK_USAGE" -ge 80 ]; then
+	echo "CRTICAL: Disk usage is at ${DISK_USAGE}%"
+	exit 1
+else
+	echo "OK: Disk usage is at ${DISK_USAGE}%"
+    exit 0
+fi
+# echo "checking system Health";
+# df -h;
+# free -m;
 
